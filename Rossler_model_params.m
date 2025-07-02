87c6b26c69581faedca8e3a0e86c32a77862f2c4
Jakub Cieślik <jakub.cieslik1999@gmail.com>
2025-07-02 14:31:11 +0200

    fixing mistake with noise samples

--- Rossler_model_params.m
+++ Rossler_model_params.m
@@ -4,10 +4,10 @@ b = 0.2;
 c = 5.7;
 
 %Simulation time
-StopTime = 200;
+StopTime = 100;
 
 %Adding noise to samples. If bit = 0 there is no noise. If bit = 1 there is
 NoiseBit = 1;
 %NoiseLevel = [0, 0.001, 0.002, 0.005, 0.01, 0.1];
 %NoiseLevel variable is amplitude of noise
-NoiseLevel = 0.002;
+NoiseLevel = 0.005;
